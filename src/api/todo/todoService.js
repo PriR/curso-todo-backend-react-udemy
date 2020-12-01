0d241335774144c3963521005cb5f3b6facb3c7a
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2020-12-01 15:16:54 +0100

    change port and fix import

--- src/api/todo/todoService.js
+++ src/api/todo/todoService.js
@@ -1,4 +1,4 @@
-const Todo = require("./to@do");
+const Todo = require("./todo");
 
 Todo.methods(["get", "post", "put", "delete"]); // a ser habilitado pela api pelo node-restful
 Todo.updateOptions({ new: true, runValidators: true }); // new: quando atualiza o cliente e recebe o antigo e não o novo, runValidators: aplicar as validações colocadas no schema
